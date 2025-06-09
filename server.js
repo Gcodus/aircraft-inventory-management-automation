736e27a3e73abb9927e6f927eaaabac31e66926a
Gee Ryan <g.ryan@hotmail.co.uk>
2025-06-09 15:26:38 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
